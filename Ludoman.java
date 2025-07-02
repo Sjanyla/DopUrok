71b50ba374f1464715ced2f76b770ac6fc925b3e
Sjanyla <Sjanyla@bk.ru>
2025-07-02 23:43:05 +0600

    Add files via upload

--- Ludoman.java
+++ Ludoman.java
@@ -3,33 +3,30 @@ import java.util.Random;
 
 public abstract class Ludoman extends Hero {
 
-    private Random random = new Random();
+    public Ludoman(int health, int damage, String name) {
+        super(health, damage, name, SuperAbility.LUDOMAN);
 
-    public Ludoman(int health, int damage, String name, SuperAbility ability, Random random) {
-        super(health, damage, name, SuperAbility);
-        this.random = random;
     }
 
-    public void useAbility(Boss boss, List<Hero> team) {
-        int die1 = random.nextInt(6) + 1;
-        int die2 = random.nextInt(6) + 1;
-        System.out.println("Ludoman : " + ", Бросает кости: " + die1 + " && " + die2);
-    }
         @Override
         public void applySuperPower(Boss boss, Hero[] heroes) {
+            System.out.println("===============LUDOMAN Super Ability");
             Random random = new Random();
 
             int firstDice = random.nextInt(6) + 1;
             int secondDice = random.nextInt(6) + 1;
+            System.out.println("Doice1: "+ firstDice+" Dice 2: "+secondDice);
 
             if (firstDice == secondDice) {
                 boss.setHealth(boss.getHealth() - (firstDice * secondDice));
+                System.out.println("Ludoman hits the boss: "+(firstDice*secondDice));
             } else {
                 int sum = firstDice + secondDice;
                 for (Hero hero : heroes) {
                     hero.setHealth(hero.getHealth() - sum);
                     break;
                 }
+                System.out.println("Ludoman hits the ally: "+(firstDice+secondDice));
             }
         }
 
